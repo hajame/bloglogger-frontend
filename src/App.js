3b0b460a0c9a9b1759bd356d0b54b74e8fbeb358
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-02-05 16:04:47 +0200

    5.3 (HY 2019) blogs can be added when logged in

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
   const [user, setUser] = useState(null)
 
@@ -32,7 +35,7 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
-      
+
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
       blogService.setToken(user.token)
       setUser(user)
@@ -52,20 +55,48 @@ const App = () => {
     window.localStorage.clear()
   }
 
+  const handleCreation = async (event) => {
+    event.preventDefault()
+    let newBlog = { title, author, url }
+    newBlog = await blogService.create(newBlog)
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    setBlogs(blogs.concat(newBlog))
+  }
+
   return (
     <div>
       <h1>Bloglogger</h1>
-      { user === null ?
+      {user === null ?
         <LoginFrom
           handleLogin={handleLogin}
           username={username}
           password={password}
           setUsername={setUsername}
-          setPassword={setPassword}         
+          setPassword={setPassword}
         /> :
         <div>
           <p>{user.name} logged in</p>
           <button onClick={handleLogout}>Log out</button>
+
+          <h2>create new</h2>
+          <form onSubmit={handleCreation}>
+            <div>title:
+              <input type="text" value={title} name="title"
+                onChange={({ target }) => setTitle(target.value)} />
+            </div>
+            <div>author:
+              <input type="text" value={author} name="author"
+                onChange={({ target }) => setAuthor(target.value)} />
+            </div>
+            <div>url:
+              <input type="text" value={url} name="url"
+                onChange={({ target }) => setUrl(target.value)} />
+            </div>
+            <button type="submit">Create</button>
+          </form>
+
           <h2>blogs</h2>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -16,6 +16,8 @@ const create = async newObject => {
   const config = {
     headers: { Authorization: token }
   }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
 const update = (id, newObject) => {
