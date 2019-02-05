836f73b0b53f892207f75f22043121736e0aba33
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-02-05 12:52:49 +0200

    started over with 2019 material

--- src/App.js
+++ src/App.js
@@ -1,90 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-import loginService from "./services/login";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      blogs: [],
-      error: null,
-      username: '',
-      password: '',
-      user: null
-    }
-  }
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-  componentDidMount() {
+  useEffect(() => {
     blogService.getAll().then(blogs =>
-      this.setState({ blogs })
+      setBlogs(blogs)
     )
-  }
+  }, [])
 
-  login = async (event) => {
+  const handleLogin = (event) => {
     event.preventDefault()
-    try {
-      const user = await loginService.login({
-        username: this.state.username,
-        password: this.state.password
-      })
-
-      this.setState({ username: '', password: '', user })
-    } catch (exception) {
-      this.setState({
-        error: 'wrong username or password',
-      })
-      setTimeout(() => {
-        this.setState({ error: null })
-      }, 5000)
-    }
+    console.log('logging in with', username, password)
   }
 
-  handleLoginFieldChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
-
-  render() {
-
-    const loginForm = () => (
-      <div>
-        <h2>Log in to application</h2>
-        <form onSubmit={this.login}>
-          <div>
-            username:
-              <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleLoginFieldChange}
-            />
-          </div>
-          <div>
-            password:
-              <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleLoginFieldChange}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    )
-
-    return (
-      <div>
-        <h1>Bloglogger</h1>
-
-        {this.state.user === null ?
-          loginForm() :
-          this.state.blogs.map(blog =>
-            <Blog key={blog._id} blog={blog} />
-          )}
-      </div>
-    )
-  }
+  return (    
+    <div>
+      <h1>Bloglogger</h1>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+      <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+      <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+      </form>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
 }
 
-export default App;
+export default App
