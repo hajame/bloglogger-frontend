2428855b34ee3143af890e50a29350971afa93a1
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-02-21 13:04:23 +0200

    5.6 (HY 2019) - clicking Blog name reveals full info

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,50 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    <a href={blog.url}>{blog.title}</a> by <i>{blog.author}</i>
-  </div>
-)
+import React, { useState, useImperativeHandle } from 'react'
+const Blog = ({ blog, ref }) => {
+
+  const [expand, setExpand] = useState(false)
+
+  const toggleExpand = () => {
+    setExpand(!expand)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleExpand
+    }
+  })
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      { expand ?
+        <div onClick={toggleExpand}>
+          <div>
+            &quot;{blog.title}&quot; by: <i>{blog.author}</i>
+          </div>
+          <div>
+            <a href={blog.url}>{blog.url}</a>
+          </div>
+          <div>
+            {blog.likes} <button>like</button>
+          </div>
+          <div>
+            added by {blog.user.name}
+          </div>
+        </div>
+        :
+        <div onClick={toggleExpand}>
+          &quot;{blog.title}&quot; by <i>{blog.author}</i>
+        </div>
+      }
+    </div>
+  )
+}
 
 export default Blog
