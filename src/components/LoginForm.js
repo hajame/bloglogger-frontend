3819f70d4b3992ef03477d242df5e78c993accad
Harri Mehtälä <harri.mehtala@helsinki.fi>
2019-02-25 17:52:02 +0200

    5.20 (HY2019) removed error - Used function variables and made reset undefined

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const LoginForm = (props) => (
-  <div>
-    <h2>Login</h2>
-    <form onSubmit={props.handleLogin}>
-      <div className='usernameField'>
-        username
-        <input
-          value={props.username.value}
-          type={props.username.type}
-          onChange={props.username.onChange}
-        />
-      </div>
-      <div>
-        password
-        <input
-          value={props.password.value}
-          type={props.password.type}
-          onChange={props.password.onChange}        
-        />
-      </div>
-      <button type='submit'>Log in</button>
-    </form>
-  </div>
-)
+const LoginForm = (props) => {
+
+  const username = { ...props.username }
+  username.reset = undefined
+  const password = { ...props.password }
+  password.reset = undefined
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={props.handleLogin}>
+        <div className='usernameField'>
+          username
+          <input {...username} />
+        </div>
+        <div>
+          password
+          <input {...password} />
+        </div>
+        <button type='submit'>Log in</button>
+      </form>
+    </div>
+  )
+}
 
 LoginForm.propTypes = {
   handleLogin: PropTypes.func.isRequired,
